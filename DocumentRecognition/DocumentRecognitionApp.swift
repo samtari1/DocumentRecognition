85c3cf8ef28ee5181dbbb7b63f75e01b313d3603
samtari1 <samtariyang@gmail.com>
2026-03-17 22:51:33 -0400

    Initial Commit

---
+++ DocumentRecognition/DocumentRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentRecognitionApp.swift
+//  DocumentRecognition
+//
+//  Created by Quanpeng Yang on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct DocumentRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DocumentRecognitionTests/DocumentRecognitionTests.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentRecognitionTests.swift
+//  DocumentRecognitionTests
+//
+//  Created by Quanpeng Yang on 3/17/26.
+//
+
+import Testing
+@testable import DocumentRecognition
+
+struct DocumentRecognitionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
